2bd53b599bff77f615007be2b2cbbfcc0959bd95
doantungbmt <doantungbmt47@gmail.com>
2021-12-05 12:17:27 +0700

    change logo

--- core/js/apps.js
+++ core/js/apps.js
@@ -9,114 +9,112 @@
  */
 
 (function (document, $, exports) {
+  "use strict";
 
-	'use strict';
+  var dynamicSlideToggleEnabled = false;
 
-	var dynamicSlideToggleEnabled = false;
+  exports.Apps = {
+    enableDynamicSlideToggle: function () {
+      dynamicSlideToggleEnabled = true;
+    },
+  };
 
-	exports.Apps = {
-		enableDynamicSlideToggle: function () {
-			dynamicSlideToggleEnabled = true;
-		}
-	};
+  /**
+   * Shows the #app-sidebar and add .with-app-sidebar to subsequent siblings
+   *
+   * @param {Object} [$el] sidebar element to show, defaults to $('#app-sidebar')
+   */
+  exports.Apps.showAppSidebar = function ($el) {
+    var $appSidebar = $el || $("#app-sidebar");
+    $appSidebar.removeClass("disappear");
+    $("#app-content")
+      .addClass("with-app-sidebar")
+      .trigger(new $.Event("appresized"));
+  };
 
-	/**
-	 * Shows the #app-sidebar and add .with-app-sidebar to subsequent siblings
-	 *
-	 * @param {Object} [$el] sidebar element to show, defaults to $('#app-sidebar')
-	 */
-	exports.Apps.showAppSidebar = function($el) {
-		var $appSidebar = $el || $('#app-sidebar');
-		$appSidebar.removeClass('disappear');
-		$('#app-content').addClass('with-app-sidebar').trigger(new $.Event('appresized'));
+  /**
+   * Shows the #app-sidebar and removes .with-app-sidebar from subsequent
+   * siblings
+   *
+   * @param {Object} [$el] sidebar element to hide, defaults to $('#app-sidebar')
+   */
+  exports.Apps.hideAppSidebar = function ($el) {
+    var $appSidebar = $el || $("#app-sidebar");
+    $appSidebar.addClass("disappear");
+    $("#app-content")
+      .removeClass("with-app-sidebar")
+      .trigger(new $.Event("appresized"));
+  };
 
-	};
+  /**
+   * Check if sidebar is visible/hidden
+   *
+   * @param {Object} [$el] sidebar element to check, defaults to $('#app-sidebar')
+   */
 
-	/**
-	 * Shows the #app-sidebar and removes .with-app-sidebar from subsequent
-	 * siblings
-	 *
-	 * @param {Object} [$el] sidebar element to hide, defaults to $('#app-sidebar')
-	 */
-	exports.Apps.hideAppSidebar = function($el) {
-		var $appSidebar = $el || $('#app-sidebar');
-		$appSidebar.addClass('disappear');
-		$('#app-content').removeClass('with-app-sidebar').trigger(new $.Event('appresized'));
-	};
+  exports.Apps.AppSidebarVisible = function ($el) {
+    var $appSidebar = $el || $("#app-sidebar");
+    return !$appSidebar.hasClass("disappear");
+  };
 
-	/**
-	 * Check if sidebar is visible/hidden
-	 *
-	 * @param {Object} [$el] sidebar element to check, defaults to $('#app-sidebar')
-	 */
+  /**
+   * Provides a way to slide down a target area through a button and slide it
+   * up if the user clicks somewhere else. Used for the news app settings and
+   * add new field.
+   *
+   * Usage:
+   * <button data-apps-slide-toggle=".slide-area">slide</button>
+   * <div class=".slide-area" class="hidden">I'm sliding up</div>
+   */
+  var registerAppsSlideToggle = function () {
+    var buttons = $("[data-apps-slide-toggle]");
 
-	exports.Apps.AppSidebarVisible = function($el) {
-		var $appSidebar = $el || $('#app-sidebar');
-		return !$appSidebar.hasClass('disappear');
-	};
+    $(document).click(function (event) {
+      if (dynamicSlideToggleEnabled) {
+        buttons = $("[data-apps-slide-toggle]");
+      }
 
-	/**
-	 * Provides a way to slide down a target area through a button and slide it
-	 * up if the user clicks somewhere else. Used for the news app settings and
-	 * add new field.
-	 *
-	 * Usage:
-	 * <button data-apps-slide-toggle=".slide-area">slide</button>
-	 * <div class=".slide-area" class="hidden">I'm sliding up</div>
-	 */
-	var registerAppsSlideToggle = function () {
-		var buttons = $('[data-apps-slide-toggle]');
+      buttons.each(function (index, button) {
+        var areaSelector = $(button).data("apps-slide-toggle");
+        var area = $(areaSelector);
 
-		$(document).click(function (event) {
+        function hideArea() {
+          area.slideUp(OC.menuSpeed * 4, function () {
+            area.trigger(new $.Event("hide"));
+          });
+        }
+        function showArea() {
+          area.slideDown(OC.menuSpeed * 4, function () {
+            area.trigger(new $.Event("show"));
+          });
+        }
 
-			if (dynamicSlideToggleEnabled) {
-				buttons = $('[data-apps-slide-toggle]');
-			}
+        // do nothing if the area is animated
+        if (!area.is(":animated")) {
+          // button toggles the area
+          if (
+            $(button).is($(event.target).closest("[data-apps-slide-toggle]"))
+          ) {
+            if (area.is(":visible")) {
+              hideArea();
+            } else {
+              showArea();
+            }
 
-			buttons.each(function (index, button) {
+            // all other areas that have not been clicked but are open
+            // should be slid up
+          } else {
+            var closest = $(event.target).closest(areaSelector);
+            if (area.is(":visible") && closest[0] !== area[0]) {
+              hideArea();
+            }
+          }
+        }
+      });
+    });
+  };
 
-				var areaSelector = $(button).data('apps-slide-toggle');
-				var area = $(areaSelector);
-
-				function hideArea() {
-					area.slideUp(OC.menuSpeed*4, function() {
-						area.trigger(new $.Event('hide'));
-					});
-				}
-				function showArea() {
-					area.slideDown(OC.menuSpeed*4, function() {
-						area.trigger(new $.Event('show'));
-					});
-				}
-
-				// do nothing if the area is animated
-				if (!area.is(':animated')) {
-
-					// button toggles the area
-					if ($(button).is($(event.target).closest('[data-apps-slide-toggle]'))) {
-						if (area.is(':visible')) {
-							hideArea();
-						} else {
-							showArea();
-						}
-
-					// all other areas that have not been clicked but are open
-					// should be slid up
-					} else {
-						var closest = $(event.target).closest(areaSelector);
-						if (area.is(':visible') && closest[0] !== area[0]) {
-							hideArea();
-						}
-					}
-				}
-			});
-
-		});
-	};
-
-
-	$(document).ready(function () {
-		registerAppsSlideToggle();
-	});
-
-}(document, jQuery, OC));
+  $(document).ready(function () {
+    registerAppsSlideToggle();
+  });
+})(document, jQuery, OC);
