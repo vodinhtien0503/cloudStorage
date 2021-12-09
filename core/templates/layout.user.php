461a6f9f76d45a40cea335bf707904f2cbe3fbe9
doantungbmt <doantungbmt47@gmail.com>
2021-12-09 14:32:04 +0700

    dec 09 2021

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -39,7 +39,7 @@
 	</head>
 	<body id="<?php p($_['bodyid']);?>" <?php
 	
-	if ($theme->getName() !== 'CloudTT') {
+	if ($theme->getName() !== 'VKU Cloud') {
 		print_unescaped('class="theme-' . \str_replace(' ', '-', $theme->getName()) . ' has-theme"');
 	} ?> >
 		<?php include('layout.noscript.warning.php'); ?>
